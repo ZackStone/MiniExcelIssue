7e64f0d0a71ce52bb8d6d2bf945b53ce1a926a9d
Zack Stone <zackstone401@gmail.com>
2024-03-10 20:20:11 -0300

    MiniExcelIssue

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MiniExcelLibs;
+
+using FileStream fileStream = File.OpenRead(@".\issue.xlsx");
+using StreamReader streamReader = new(fileStream);
+
+var rows = MiniExcel.Query(streamReader.BaseStream, useHeaderRow: true);
+
+for (int i = 0; i < rows.Count(); i++)
+{
+    Console.WriteLine($"================= ROW {i} =================");
+
+    var row = rows.ElementAt(i);
+
+	foreach (var cell in row)
+	{
+        Console.WriteLine(cell);
+    }
+}
+
